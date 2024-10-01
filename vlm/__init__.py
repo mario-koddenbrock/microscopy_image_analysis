2e9a64aa8ffb6cebb18e63bdd8aac5c92943fec2
koddenbrock <koddenbrock@gmail.com>
2024-10-01 15:46:15 +0200

    added init to ignore some unimportant warnings

---
+++ vlm/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+import rasterio
+
+# Ignore specific warnings
+ignored_warnings = [
+    rasterio.errors.NotGeoreferencedWarning,
+    DeprecationWarning,
+    FutureWarning,
+    UserWarning
+]
+
+for warning in ignored_warnings:
+    warnings.filterwarnings("ignore", category=warning)
