a1f5c877b70e4fa98ea5c7eb9179ec3b443d0613
koddenbrock <koddenbrock@gmail.com>
2024-10-01 15:49:28 +0200

    refactored and code clean up

---
+++ vlm_classification.py
@@ -0,0 +1,49 @@
+import torch
+
+from plotting import plot_image_grid
+from utils import initialize_evaluators, evaluate_dataset
+from vlm.blip_2 import BLIP2Evaluator
+from vlm.blip import BLIPEvaluator
+from vlm.clip import CLIPEvaluator
+from vlm.pali_gemma import PaliGemmaEvaluator
+from vlm.phi_vision import PhiVisionEvaluator
+from vlm.qwen_vl import QwenVLEvaluator
+from vlm.vilt import ViltEvaluator
+from vlm.visual_bert import VisualBertEvaluator
+
+
+if __name__ == "__main__":
+    # Determine the device to use (GPU if available, otherwise CPU)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Device: {device}")
+
+    # Configuration for evaluators
+    EVALUATORS_CONFIG = {
+        "CLIP": CLIPEvaluator,
+        # "PaliGemma": PaliGemmaEvaluator,
+        # "PhiVision": PhiVisionEvaluator,
+        # "QwenVL": QwenVLEvaluator,
+        # "Vilt": ViltEvaluator,
+        # "VisualBert": VisualBertEvaluator,
+        # "BLIP": BLIPEvaluator,
+        # "BLIP2": BLIP2Evaluator,
+    }
+
+    # Initialize evaluators
+    evaluators = initialize_evaluators(device, EVALUATORS_CONFIG)
+
+
+    dataset_name = 'Digital Image of Bacterial Species'
+    dataset_description = '''
+    The dataset from the study "Deep learning approach to bacterial colony classification" comprises 660 images representing 33 different genera and species of bacteria. 
+    This dataset, called DIBaS (Digital Image of Bacterial Species), was created for bacterial classification using deep learning methods. 
+    The images were taken with a microscope and analyzed using Convolutional Neural Networks (CNNs) and machine learning classifiers like Support Vector Machines (SVM) and Random Forest. 
+    The dataset is publicly available for research purposes, allowing for advancements in bacterial recognition systems.
+    '''
+    dataset_path = 'datasets/Classification'
+    num_images_per_class = 1
+    num_classes = 33
+
+    results = evaluate_dataset(evaluators, dataset_name, dataset_description, dataset_path, num_images_per_class, num_classes)
+
+    plot_image_grid(results, num_classes)
