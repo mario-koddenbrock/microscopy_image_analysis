3a835da780d0a50b740f915688b6eb49c7570404
koddenbrock <koddenbrock@gmail.com>
2024-12-15 14:23:40 +0100

    improved 3d cellpose vizualization

--- mia/viz.py
+++ mia/viz.py
@@ -3,7 +3,74 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from napari_animation import Animation
+from tqdm import tqdm
 
+
+# import matplotlib
+# matplotlib.use('TkAgg')
+
+
+def extract_cellpose_video(viewer, output_dir, video_filename, num_z_slices, mode='2D', rotation_steps=72):
+    """
+    Create a video from a Napari viewer, with optional 2D z-slice animation or 3D rotation.
+
+    Parameters:
+        viewer: napari.Viewer
+            The Napari viewer instance.
+        output_dir: str
+            Directory to save the video.
+        video_filename: str
+            Name of the output video file.
+        num_z_slices: int
+            Number of z-slices for the 2D animation.
+        mode: str
+            Animation mode, either '2D' or '3D'. Defaults to '2D'.
+        rotation_angle: int
+            Total angle (in degrees) to rotate in 3D mode. Defaults to 360 degrees.
+        rotation_steps: int
+            Number of steps (frames) for the 3D rotation animation. Defaults to 36.
+    """
+    video_path = os.path.join(output_dir, video_filename)
+
+    # Set FPS based on mode
+    fps = 100 if mode == '2D' else 30  # Higher FPS for 2D, lower for 3D
+
+    # Create an animation object
+    animation = Animation(viewer)
+
+    if mode == '2D':
+        # 2D animation: iterate through z-slices
+        for z in tqdm(range(int(num_z_slices / 2)), desc="Capturing 2D frames"):
+            viewer.dims.set_point(0, 2 * z)  # Set the z-slice
+            animation.capture_keyframe()
+
+    elif mode == '3D':
+        # Ensure viewer is in 3D mode
+        viewer.dims.ndisplay = 3
+
+        # 3D animation: rotate the camera
+        for i in tqdm(range(rotation_steps), desc="Capturing 3D frames"):
+            azimuth = (i * 360) / rotation_steps
+            viewer.camera.angles = (0, azimuth, 0)  # Adjust elevation and azimuth
+            animation.capture_keyframe()
+
+    else:
+        raise ValueError("Invalid mode. Use '2D' or '3D'.")
+
+    # Save the animation
+    animation.animate(video_path, canvas_only=True, fps=fps, quality=9)
+    print(f"Saved animation to {video_path}")
+
+
+def plot_intensety(image):
+    plt.hist(image.flatten(), bins=1000)
+    # make log scale
+    plt.yscale('log')
+    plt.ylabel('count')
+    plt.xlabel('intensity')
+    plt.title('intensity distribution')
+    plt.show()
 
 def save_as_video(output_video_path, image_with_labels, labels, regions):
     # Get the number of unique labels (excluding the background)
