bff132ef3653f76745c7b13e8228cf8cdde2b076
koddenbrock <koddenbrock@gmail.com>
2024-12-15 14:25:46 +0100

    setting ffmpg path in init

--- mia/__init__.py
+++ mia/__init__.py
@@ -1,4 +1,7 @@
+import os
 import warnings
+
+import cellpose
 import rasterio
 
 # Ignore specific warnings
@@ -11,3 +14,7 @@ ignored_warnings = [
 
 for warning in ignored_warnings:
     warnings.filterwarnings("ignore", category=warning)
+
+# Set the path to the ffmpeg executable - only needed for exporting animations
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/bin/ffmpeg"
+print(f"Cellpose version: {cellpose.version}")
