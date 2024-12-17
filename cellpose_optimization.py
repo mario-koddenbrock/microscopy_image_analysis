83cc37eccb20618d2623154610287ff6875b5cb8
koddenbrock <koddenbrock@gmail.com>
2024-12-17 13:44:10 +0100

    implemented more metricees

--- cellpose_optimization.py
+++ cellpose_optimization.py
@@ -2,15 +2,27 @@ import glob
 import os
 
 import napari
-from cellpose import models, io
+
+from cellpose import io
+from cellpose.metrics import boundary_scores, mask_ious
+from cellpose.models import Cellpose
+from segmentation_models_pytorch.utils.functional import jaccard
+from skimage.metrics import adapted_rand_error
+from tqdm import tqdm
 
 import wandb
 from mia.cellpose import evaluation_params, evaluate_model
 from mia.file_io import get_cellpose_ground_truth
 from mia.hash import compute_hash
-from mia.metrics import simple_iou
+from mia.metrics import simple_iou, jaccard_score, f1_score
 from mia.results import ResultHandler
-from mia.utils import check_set_gpu, check_paths
+from mia.utils import check_paths, check_set_gpu
+from mia.viz import plot_intensity, show_napari
+
+
+# TODO use CellposeModel instead of Cellpose class
+
+# setting PYTORCH_ENABLE_MPS_FALLBACK=1
 
 
 def optimize_parameters(
@@ -19,70 +31,42 @@ def optimize_parameters(
         cache_dir: str = "cache",
         type:str = "Nuclei",  # "Nuclei" or "Membranes"
         log_wandb: bool = False,
+        show_viewer: bool = False,
 ):
 
-    check_paths(image_dir, output_dir, cache_dir)
-
-    # Do you want to show the viewer?
-    show_viewer = False
-
-    # get available torch device (CPU, GPU or MPS)
-    device = check_set_gpu()
-
-    # Load the Cellpose model
-    model_args = {
-        "device": device,
-        "nchan": 2, # TODO check if this is correct = 1?
-        # "diam_mean": 30, # TODO how to change this?
-        "gpu": False,
-    }
-
-    model_dict = {
-        "cyto": models.Cellpose(model_type='cyto', **model_args),
-        "cyto2": models.Cellpose(model_type='cyto2', **model_args),
-        "cyto3": models.Cellpose(model_type='cyto3', **model_args),
-        "nuclei": models.Cellpose(model_type='nuclei', **model_args),
-    }
-
-    # Get the list of images
-    image_paths = glob.glob(os.path.join(image_dir, "*.tif"))
-
-    result_path = os.path.join(output_dir, "results.csv")
-    if os.path.exists(result_path):
-        os.remove(result_path)
-
-    if type == "Nuclei":
-        channel_idx = 0
-    elif type == "Membranes":
-        channel_idx = 1
-    else:
-        raise ValueError(f"Invalid type: {type}")
-
-
+    device = check_set_gpu() # get available torch device (CPU, GPU or MPS)
+    result_path = check_paths(image_dir, output_dir, cache_dir)
+    image_paths = glob.glob(os.path.join(image_dir, "*.tif")) # Get the list of images
     result_handler = ResultHandler(result_path)
 
     # Loop over the images
-    for image_idx, image_path in enumerate(image_paths[1:]):
+    for image_idx, image_path in enumerate(image_paths):
 
         image_name = os.path.basename(image_path).replace(".tif", "")
 
         # Read image with cellpose.io
-        image = io.imread(image_path)
+        image_orig = io.imread(image_path)
         ground_truth = get_cellpose_ground_truth(image_path, image_name)
 
-        # # TODO: contrast normalization on image
-        # image = rescale_intensity(image)
 
-        if image.ndim == 4:
-            image = image[:, channel_idx, :, :]
+        for param_idx, params in enumerate(evaluation_params):
 
-        # plot the intensety distribution of the image
-        # plot_intensety(image)
+            type = params["type"]
+            if type == "Nuclei":
+                channel_idx = 0
+            elif type == "Membranes":
+                channel_idx = 1
+            else:
+                raise ValueError(f"Invalid type: {type}")
 
-        for params in evaluation_params:
+            # Get the right channel
+            image = image_orig[:, channel_idx, :, :] if image_orig.ndim == 4 else image_orig
 
-            # if result_handler.is_result_present(params):
-            #     continue  # Skip if result already exists
+            # # TODO: contrast normalization on image
+            # image = rescale_intensity(image)
+
+            # plot the intensity distribution of the image
+            # plot_intensity(image)
 
             cache_key = compute_hash(image, params)
             if log_wandb:
@@ -92,21 +76,39 @@ def optimize_parameters(
                     config=params,
                 )
 
-            model = model_dict[params["model_name"]]
-            print(f"Processing image {image_name} ({image.shape}) with parameters: {params}")
+            # Load the Cellpose model
+            model = Cellpose(
+                model_type = params["model_name"],
+                device = device,
+                nchan = 2,  # TODO check if this is correct = 1?
+                # diam_mean = 30, # TODO how to change this?
+                gpu = False,
+            )
+
+            print(f"{image_idx+1}/{len(image_paths)}: Processing image {image_name} {image.shape}")
+            print(f"\t{param_idx+1}/{len(evaluation_params)}: Parameter:")
+            # for k, v in params.items():
+            #     print(f"\t\t{k}: {v}")
 
             masks, flows, styles, diams = evaluate_model(model, image, params, cache_dir)
-
-            simple_jaccard = simple_iou(ground_truth, masks)
-            # jaccard = jaccard_score(ground_truth, masks)
-            # fscore = f1_score(ground_truth, masks)
-            # precision, recall, fscore = boundary_scores(ground_truth, masks)
 
             if masks is None:
                 print(f"Error: No masks found for {image_name} with parameters: {params}")
                 continue
 
-            print(f"Simple Jaccard: {simple_jaccard}")
+            else:
+                # jaccard = jaccard_score(ground_truth, masks)
+                # simple_jaccard = simple_iou(ground_truth, masks)
+                # iout, preds = mask_ious(ground_truth, masks)
+                # jaccard = jaccard_score(ground_truth, masks)
+                # fscore = f1_score(ground_truth, masks)
+
+                are, precision, recall = adapted_rand_error(ground_truth, masks)
+                f1 = 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else 0
+                print(f"\tAdapted Rand Error: {are:.2f}")
+                print(f"\tPrecision: {precision:.2f}")
+                print(f"\tRecall: {recall:.2f}")
+                print(f"\tF1: {f1:.2f}")
 
             # fig = plt.figure(figsize=(12, 5))
             # plot.show_segmentation(fig, image, masks, flows, channels=[0, 0])
@@ -115,46 +117,19 @@ def optimize_parameters(
             if log_wandb:
                 wandb.log({
                     **params,
-                    "simple_jaccard": simple_jaccard,
+                    "adapted_rand_error": are,
+                    "precision": precision,
+                    "recall": recall,
+                    "f1": f1,
                     "image_name": image_name,
                     "image_idx": image_idx,
                 })
+                wandb.finish()
 
-            result_handler.log_result(params, simple_jaccard, -1)
+            result_handler.log_result(params, are, precision, recall, f1)
 
             if show_viewer:
-                # Initialize the Napari viewer
-                viewer = napari.Viewer()
-
-                # Add the image to the viewer
-                viewer.add_image(
-                    image,
-                    contrast_limits=[113, 1300],
-                    name='Organoids',
-                    colormap='gray',
-                )
-
-                # Add the labels to the viewer
-                viewer.add_labels(
-                    masks,
-                    name=params["model_name"],
-                    opacity=0.8,
-                    blending='translucent',
-                )
-
-                # setting the viewer to the center of the image
-                center = image.shape[0] // 2
-                viewer.dims.set_point(0, center)
-
-                napari.run()
-
-        if image_idx > 10:
-            break
-
-    if log_wandb:
-        wandb.finish()
-
-
+                show_napari(image, ground_truth, masks, params)
 
 
 
@@ -165,7 +140,7 @@ if __name__ == "__main__":
     # get all the subfolder
     subfolders = glob.glob(os.path.join(main_folder, "*"))
 
-    for folder in subfolders:
+    for folder in subfolders[1:]:
 
         if not os.path.isdir(folder):
             continue
