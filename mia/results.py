b432463144bdff2149e6ab4fce46c830cb983be7
koddenbrock <koddenbrock@gmail.com>
2024-12-17 13:40:07 +0100

    fixed results logging

--- mia/results.py
+++ mia/results.py
@@ -1,94 +1,45 @@
 import csv
 import os
 
+from prettytable import PrettyTable
+
 
 class ResultHandler:
     def __init__(self, result_path):
         self.result_path = result_path
-        self.fieldnames = [
-            "model_name",
-            "channel_segment",
-            "channel_nuclei",
-            "channel_axis",
-            "invert",
-            "normalize",
-            "diameter",
-            "do_3D",
-            "jaccard",
-            "f1_score",
-        ]
-        self.existing_results = []
 
-
-    def _load_existing_results(self):
-        """Load existing results from the CSV file into a set."""
-        if os.path.exists(self.result_path):
-            with open(self.result_path, mode="r", newline="") as csv_file:
-                reader = csv.DictReader(csv_file)
-                for row in reader:
-                    self.existing_results.append(self.get_result_key_from_row(row))
-
-    def log_result(self, config, jaccard, f1_score):
+    def log_result(self, properties, are, precision, recall, f1):
         """Log a new result to the CSV file."""
-        result_key = self.get_result_key_from_params(config)
-        if result_key in self.existing_results:
-            return  # Skip duplicate results
+        properties['are'] = are
+        properties['precision'] = precision
+        properties['recall'] = recall
+        properties['f1'] = f1
 
-        # Build the result dictionary
-        result = self.build_result_dict(config, jaccard, f1_score)
+        fieldnames = list(properties.keys())
+
+        file_exists = os.path.exists(self.result_path)
 
         with open(self.result_path, mode="a", newline="") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
-            if os.stat(self.result_path).st_size == 0:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            if not file_exists or os.stat(self.result_path).st_size == 0:
                 writer.writeheader()
-            writer.writerow(result)
-        self.existing_results.append(result_key)
+            writer.writerow(properties)
 
-    def is_result_present(self, config):
-        """Check if a result already exists."""
-        current_result_key = self.get_result_key_from_params(config)
-        # print(current_result_key)
-        return current_result_key in self.existing_results
+        self.print_results()
 
-    def build_result_dict(self, params, jaccard, f1_score):
-        """Build the result dictionary for logging."""
-        result = {
-            "model_name": params["model_name"],
-            "channel_segment": params["channel_segment"],
-            "channel_nuclei": params["channel_nuclei"],
-            "channel_axis": params["channel_axis"],
-            "invert": params["invert"],
-            "normalize": params["normalize"],
-            "diameter": params["diameter"],
-            "do_3D": params["do_3D"],
-            "jaccard": jaccard,
-            "f1_score": f1_score,
-        }
+    def print_results(self):
+        """Print the results as a pretty table."""
+        if not os.path.exists(self.result_path):
+            print("No results to display.")
+            return
 
-        return result
-
-    def get_result_key_from_params(self, params):
-        """Return a unique identifier for a params dict."""
-        return (
-            params["model_name"],
-            params["channel_segment"],
-            params["channel_nuclei"],
-            params["channel_axis"],
-            params["invert"],
-            params["normalize"],
-            params["diameter"],
-            params["do_3D"],
-        )
-
-    def get_result_key_from_row(self, row):
-        """Return a unique identifier for a row in the CSV file."""
-        return (
-            row["model_name"],
-            eval(row["channel_segment"]),
-            eval(row["channel_nuclei"]),
-            eval(row["channel_axis"]),
-            eval(row["invert"]),
-            eval(row["normalize"]),
-            eval(row["diameter"]),
-            eval(row["do_3D"]),
-        )
+        with open(self.result_path, mode="r", newline="") as csv_file:
+            reader = csv.DictReader(csv_file)
+            table = PrettyTable()
+            table.field_names = reader.fieldnames
+            for row in reader:
+                formatted_row = [
+                    f"{float(row[field]):.2f}" if field in ['are', 'precision', 'recall', 'f1'] else row[field] for
+                    field in reader.fieldnames]
+                table.add_row(formatted_row)
+            print(table)
