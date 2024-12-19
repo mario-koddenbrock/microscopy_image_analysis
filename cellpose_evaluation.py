a42a3d42d843513bed98a05db55f3d805dcb8fbc
koddenbrock <koddenbrock@gmail.com>
2024-12-19 14:24:02 +0100

    return if there is no result yet

--- cellpose_evaluation.py
+++ cellpose_evaluation.py
@@ -1,13 +1,16 @@
-import glob
 import os
 
-from mia.results import ResultHandler, print_best_config_per_image
+from mia.results import print_best_config_per_image
 from mia.viz import plot_aggregated_metric_variation, plot_best_scores_barplot
 
 main_folder = "Datasets/P013T/"
 
 # get all the subfolder
 result_path = os.path.join(main_folder, "results.csv")
+
+if not os.path.exists(result_path):
+    print("No results to display.")
+    exit()
 
 print_best_config_per_image(result_path, metric='f1', output_file=result_path.replace('results.csv', 'best_configs.json'))
 plot_aggregated_metric_variation(result_path, metric='f1', boxplot=True)
