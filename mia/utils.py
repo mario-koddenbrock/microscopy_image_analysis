032e5778ff3eac11bc2ecd3e4c5a7cc3758808c6
koddenbrock <koddenbrock@gmail.com>
2024-12-19 14:24:30 +0100

    fixed divice selection

--- mia/utils.py
+++ mia/utils.py
@@ -13,14 +13,10 @@ def check_set_gpu(override=None):
     if override is None:
         if torch.cuda.is_available():
             device = torch.device('cuda')
-            print(f"Using GPU: {torch.cuda.get_device_name(0)}")
-            # TODO MPS currently not supported
-        # elif torch.backends.mps.is_available():
-        #     device = torch.device('mps')
-        #     print(f"Using MPS: {torch.backends.mps.is_available()}")
+        elif torch.backends.mps.is_available():
+            device = torch.device('mps')
         else:
             device = torch.device('cpu')
-            print(f"Using CPU: {torch.device('cpu')}")
     else:
         device = torch.device(override)
 
